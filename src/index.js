62ece837e5e9f0e8d758d40747601992271c7882
Avery King <avery.n.king@wmich.edu>
2022-02-13 15:27:40 -0500

    ``toggleStageInteractive`` added

--- src/index.js
+++ src/index.js
@@ -1,42 +1,53 @@
 import * as PIXI from 'pixi.js';
+import {BlurFilter} from 'pixi-filters';
+
 const app = new PIXI.Application({
-    width:800,
-    height:500,
+    width: 800,
+    height: 500,
     backgroundColor: 0xc9c9c9,
-    antialias:true
+    antialias: true
 })
+let stageInteractive = true;
+
+const toggleStageInteractive = () => {
+    for (let i = 0; i < colorButtons.length; i++) {
+        Object.values(colorButtons[i])[0].interactive = !stageInteractive;
+    }
+    background.interactive = !stageInteractive;
+    stageInteractive = !stageInteractive;
+}
 app.view.style.border = '1px solid black';
 app.view.style.borderRadius = '8px'
 const artBoard = new PIXI.Container;
 const background = new PIXI.Graphics();
 background.interactive = true;
 background.beginFill(0xffffff);
-background.drawRect(35,0,730,500);
-background.position.set(35,0);
-background.lineStyle(2,0x000000)
-    .moveTo(35,0)
-    .lineTo(35,500)
+background.drawRect(35, 0, 730, 500);
+background.position.set(35, 0);
+background.lineStyle(0, 0x000000)
+    .moveTo(35, 0)
+    .lineTo(35, 500)
 background.endFill();
 background.drawing = false;
-const draw = ({x,y}) => {
+const draw = ({x, y}) => {
     const point = new PIXI.Graphics();
     point.beginFill(selectedColor._fillStyle.color);
-    point.lineStyle(1,selectedColor._fillStyle.color);
-    point.drawCircle(x,y,4,4);
+    point.lineStyle(1, selectedColor._fillStyle.color);
+    point.drawCircle(x, y, 4, 4);
     point.endFill();
     app.stage.addChild(point);
 }
 
 background.mousedown = (e) => {
-    if(!selectedColor) {
+    if (!selectedColor) {
         return alert('Please select a color');
     }
     background.drawing = true;
     draw(e.data.getLocalPosition(app.stage));
 }
 
-background.mousemove = (e) => {
-    if(background.drawing) {
+background.pointermove = (e) => {
+    if (background.drawing) {
         draw(e.data.getLocalPosition(app.stage));
     }
 }
@@ -45,40 +56,40 @@ background.mouseup = () => {
 }
 
 const colors = [
-    { name:"black", color:"000000"},
-    { name:"red", color:"ff0000"},
-    { name:"blue", color:"0036FF"},
-    { name:"green", color:"087B00"},
-    { name:"yellow", color:"FFDC00"},
-    { name:"purple", color:"AD00FF"},
+    {name: "black", color: "000000"},
+    {name: "red", color: "ff0000"},
+    {name: "blue", color: "0036FF"},
+    {name: "green", color: "087B00"},
+    {name: "yellow", color: "FFDC00"},
+    {name: "purple", color: "AD00FF"},
 ]
 const colorButtons = [];
 let firstYPosition = 30;
 let selectedColor = undefined;
 const updateSelectedColor = (color) => {
-    if(selectedColor) selectedColor.removeSelected();
+    if (selectedColor) selectedColor.removeSelected();
     selectedColor = color;
     color.makeSelected();
     console.log(color)
 }
-for(let i=0; i<colors.length; i++) {
+for (let i = 0; i < colors.length; i++) {
     const color = new PIXI.Graphics();
     color.interactive = true;
     color.buttonMode = true;
     color.beginFill(`0x${colors[i].color}`);
-    color.lineStyle(4,0xc9c9c9);
-    color.drawCircle(30,firstYPosition + 50*i,20,20);
+    color.lineStyle(4, 0xc9c9c9);
+    color.drawCircle(30, firstYPosition + 50 * i, 20, 20);
     color.mouseover = () => {
         color.clear();
         color.beginFill(`0x${colors[i].color}`)
-        color.lineStyle(2,0x00FF39);
-        color.drawCircle(30,firstYPosition + 50*i,20,20);
+        color.lineStyle(2, 0x00FF39);
+        color.drawCircle(30, firstYPosition + 50 * i, 20, 20);
     }
     color.mouseout = () => {
         color.clear();
         color.beginFill(`0x${colors[i].color}`)
-        color.lineStyle(4,0xc9c9c9);
-        color.drawCircle(30,firstYPosition + 50*i,20,20);
+        color.lineStyle(4, 0xc9c9c9);
+        color.drawCircle(30, firstYPosition + 50 * i, 20, 20);
     }
 
     color.makeSelected = () => color.x += 10;
@@ -95,6 +106,39 @@ for(let i=0; i<colors.length; i++) {
     }
     app.stage.addChild(color);
 }
+
+const clearButton = new PIXI.Graphics();
+
+const clearText = new PIXI.Text('CLEAR', new PIXI.TextStyle({fontSize: 16, fontWeight: 550}));
+clearButton.lineStyle(0, 0x000000);
+clearButton.drawRect(0, 0, 50, 20);
+clearButton.addChild(clearText);
+clearButton.buttonMode = true;
+clearButton.interactive = true;
+clearButton.x = 7;
+clearButton.y = colors.length * 50 + 50;
+clearButton.mouseover = () => {
+    clearButton.clear();
+    clearButton.lineStyle(0.8, 0x000000);
+    clearButton.drawRect(-3, -3, 62, 24);
+}
+clearButton.mouseout = () => {
+    clearButton.clear();
+    clearButton.drawRect(-3, -3, 62, 24);
+}
+const confirmClear = () => {
+    const overlay = new PIXI.Graphics();
+    overlay.beginFill(0x000000);
+    overlay.drawRect(0, 0, 1000, 1000);
+    overlay.width = 1000;
+    overlay.height = 1000;
+    overlay.alpha = 0.3;
+    overlay.endFill();
+    app.stage.addChild(overlay);
+    toggleStageInteractive();
+}
+clearButton.click = () => confirmClear();
+app.stage.addChild(clearButton);
 artBoard.addChild(background);
 app.stage.addChild(artBoard);
 document.body.appendChild(app.view)
